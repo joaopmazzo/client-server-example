d06687fa47eea5039c072bee3bb8d1eb3f665236
João Paulo Mazzo <mazzojp@gmail.com>
2022-05-31 19:38:55 -0300

    Update server adding express and cors library

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,35 +1,26 @@
-import { createServer } from 'http';
-import { parse } from 'querystring';
+import express from 'express';
+import cors from 'cors';
 
-const server = createServer((request, response) => {
-  switch (request.url) {
-    case '/status': {
-      response.writeHead(200, {
-        'Content-Type': 'application/json'
-      });
-      response.write(JSON.stringify({
-        status: 'ok',
-      }));
-      response.end();
-      break;
-    }
-    case '/authenticate': {
-      let data = '';
-      request.on('data', (chunk) => {
-        data += chunk;
-      })
-      request.on('end', () => {
-        const params = parse(data);
-        response.end();
-      })
-      break;
-    }
-    default: {
-      response.writeHead(404, 'Service not found.');
-      response.end();
-    }
-  }
-});
+const server = express();
+const enableCors = cors({ origin: 'http://localhost:3000' })
+
+server.get('/status', (_, response) => {
+  response.send({
+    status: 'Okay',
+  })
+})
+
+server
+  .options('/authenticate', enableCors)
+  .post('/authenticate', enableCors, express.json(), (request, response) => {
+    console.log(
+      'E-mail', request.body.email,
+      'Senha', request.body.password
+    );
+    response.send({
+      Okay: true,
+    });
+  })
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
