34af2e563cc99ba9a047c9d3e73f3c7fb958adae
João Paulo Mazzo <mazzojp@gmail.com>
2022-06-05 14:19:01 -0300

    feat: implements our client resolvers

--- packages/server/src/graphql/resolvers.js
+++ packages/server/src/graphql/resolvers.js
@@ -1,9 +1,12 @@
+import { resolvers as clientResolvers } from "./Client/Client";
 import { resolvers as demandResolvers } from "./Demand/Demand";
 
 const resolvers = {
+  ...clientResolvers,
   ...demandResolvers,
 
   Query: {
+    ...clientResolvers.Query,
     ...demandResolvers.Query,
   }
 }
