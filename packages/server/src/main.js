fd8a6cb575d7fc90051df84edee494d3a88b199c
João Paulo Mazzo <mazzojp@gmail.com>
2022-06-05 01:52:58 -0300

    update: implements our grapql structure

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,30 +1,28 @@
 import express from 'express';
-import cors from 'cors';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-const server = express();
-const enableCors = cors({ origin: 'http://localhost:3000' })
-
-server.get('/status', (_, response) => {
-  response.send({
-    status: 'Okay',
-  })
-})
-
-server
-  .options('/authenticate', enableCors)
-  .post('/authenticate', enableCors, express.json(), (request, response) => {
-    console.log(
-      'E-mail', request.body.email,
-      'Senha', request.body.password
-    );
-    response.send({
-      Okay: true,
-    });
-  })
+import typeDefs from './graphql/typeDefs';
+import resolvers from './graphql/resolvers';
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
 
-server.listen(PORT, HOSTNAME, () => {
-  console.log(`Server is listening at http://${HOSTNAME}:${PORT}.`);
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.start().then(res => {
+  server.applyMiddleware({
+    app,
+    cors: {
+      origin: 'http://localhost:3000',
+      origin: 'https://studio.apollographql.com'
+    },
+    bodyParserConfig: true,
+  })
+  app.listen(PORT, HOSTNAME, () => {
+    console.log(`Server is listening at http://${HOSTNAME}:${PORT}.`);
+  })
 })
