ad6787c8b9b7244832a72e0b3c46a561624bd81d
João Paulo Mazzo <mazzojp@gmail.com>
2022-06-05 21:21:18 -0300

    update: rename folder and fix the write Promisse variables

--- packages/server/src/io/Database/createRepository.js
+++ packages/server/src/io/Database/createRepository.js
@@ -14,7 +14,7 @@ function createRepository(name) {
         resolve(JSON.parse(data));
       })
     }),
-    write: (data) => new Promise(() => {
+    write: (data) => new Promise((resolve, reject) => {
       writeFile(path, JSON.stringify(data), (error) => {
         if (error) {
           reject(error);
