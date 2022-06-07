ddc17163f689faadd31c12c6b1b17ba371d4beea
João Paulo Mazzo <mazzojp@gmail.com>
2022-06-07 18:53:40 -0300

    feat: implements our apollo client in front-end

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+import { ApolloProvider } from 'react-apollo';
+import client from './plugins/apollo/client';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
